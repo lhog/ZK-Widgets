2d0d85a918aef12b6296f749bbce9ca53efcc979
LHoG <ccna@mail.ru>
2018-01-25 17:31:13 +0300

    Who did comment out stardust???

--- terraform_automation.lua
+++ terraform_automation.lua
@@ -77,7 +77,7 @@ unitsToWall[-aegis.id]={h=40}
 unitsToWall[-storage.id]={}
 
 local unitsToRaise={}
---unitsToRaise[-stardust.id]={h=150}
+unitsToRaise[-stardust.id]={h=150}
 unitsToRaise[-newton.id]={h=90}
 unitsToRaise[-faraday.id]={h=90}
 unitsToRaise[-razor.id]={h=30}
